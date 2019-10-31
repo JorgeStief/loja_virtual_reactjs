0b5f8ba958f8d36c156f5c0c7459d64cb4a36193
Jorge <jorge.stief@gmail.com>
2019-10-31 00:55:24 -0300

    pages created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">Coluna numero 1</div>
-        <div className="col-6">
-          <span>
-            <i class="fas fa-home" aria-hidden="true"></i>
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Switch>
+
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        
+        <Route component={Default}></Route>
+      </Switch>
+      
+      
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class campo extends Component {
+    render(){
+        return(
+          <div>
+              <h3>Page not Found</h3>
+          </div>
+        )
+    }
+}
